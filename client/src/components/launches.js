0a6d6e9db53bcc4ee0d965bd7a5e7cd54da04fe3
ibe.eliot <41167569+ibeeliot@users.noreply.github.com>
2020-03-03 09:11:53 -0800

    Got launches to work using launchitem component. GraphQL query using the grahql tag module and the query component from apollo successfully rendered launch data from spacex api

--- client/src/components/launches.js
+++ client/src/components/launches.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
-
+import LaunchItem from './LaunchItem';
+// this is the string query you write in gql tag and use the react-apollo client in order
+// to make the queries you need. Pass the "query" down into the Query component from react-apollo
 const LAUNCHES_QUERY = gql`
 	{
 		launches {
@@ -16,18 +18,22 @@ const LAUNCHES_QUERY = gql`
 export class Launches extends Component {
 	render() {
 		return (
-			<div>
+			<>
 				<h1 className='display-4 my-3'>Launches</h1>
 				<Query query={LAUNCHES_QUERY}>
 					{({ loading, error, data }) => {
 						if (loading) return <h4>Loading...</h4>;
 						if (error) console.log(error);
-						console.log(data);
-
-						return <h1>test</h1>;
+						return (
+							<>
+								{data.launches.map(launch => (
+									<LaunchItem key={launch.flight_number} launch={launch} />
+								))}
+							</>
+						);
 					}}
 				</Query>
-			</div>
+			</>
 		);
 	}
 }
--- server.js
+++ server.js
@@ -3,10 +3,14 @@ const path = require('path');
 const graphqlHTTP = require('express-graphql');
 const PORT = process.env.PORT || 1337;
 const schema = require('./schema');
+const cors = require('cors');
 
 require('dotenv').config();
 
 const app = express();
+
+// Allow cross-origin
+app.use(cors());
 
 app.use(
 	'/graphql',
